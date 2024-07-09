9519c60370383eb72ed67e237f6ffc3c9d7d3906
wanglei <leiwang0119@163.com>
2024-07-10 00:11:07 +0800

    feat: list-data product-data category-data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,50 @@
-"use client";
+// "use client";
 
 import CategoriyList from "@/components/CategoriyList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
-import { useContext, useEffect } from "react";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense, useContext, useEffect } from "react";
 
-const HomePage = () => {
-  const wixClient = useContext(WixClientContext);
+const HomePage = async () => {
+  // const wixClient = useWixClient();
 
-  useEffect(() => {
-    const getProducts = async () => {
-      const res = await wixClient.products.queryProducts().find();
-      console.log(res);
-    };
-    getProducts();
-  }, [wixClient]);
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+  //     console.log(res);
+  //   };
+  //   getProducts();
+  // }, [wixClient]);
+
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products.queryProducts().find();
 
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={"loading"}>
+          <ProductList
+            categoryId={process.env.NEXT_PUBLIC_FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
-        <CategoriyList />
+        <Suspense fallback={"loading"}>
+          <CategoriyList />
+        </Suspense>
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
+        {/* <ProductList /> */}
       </div>
     </div>
   );
--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -1,1 +1,28 @@
-export const wixClientServer = {}
+import { OAuthStrategy, createClient } from "@wix/sdk";
+import { collections, products } from "@wix/stores";
+import { cookies } from "next/headers";
+
+
+export const wixClientServer = async () => {
+  let refreshToken
+  try{
+    const cookieStore = cookies();
+    refreshToken = JSON.parse(cookieStore.get("refreshToken")?.value || "{}");
+  }catch(e){}
+
+  const wixClient = createClient({
+    modules: {
+      products,
+      collections,
+      // currentCart,
+    },
+    auth: OAuthStrategy({
+      clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+      tokens: {
+        refreshToken,
+        accessToken: { value: "", expiresAt: 0 },
+      },
+    }),
+  });
+  return wixClient;
+}
